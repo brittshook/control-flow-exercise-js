60223a9aa47d26cb8fc878201dcd46a8aa33668e
Britt Shook <brittanysshook@gmail.com>
2023-11-22 17:23:59 -0500

    add logic for second exercise and relevant funcs to handle

--- index.js
+++ index.js
@@ -3,24 +3,26 @@
 const areaOfGardenInSqrMeters = (5 ** 2) * Math.PI;
 const minSpacePerPlantInSqrMeters = 0.8;
 const startNumOfPlants = 20;
-let plantMultiplierPerWeek = 2;
+const plantMultiplierPerWeek = 2;
 
 // Calculate max capacity of garden
-const maxCapacityOfPlants = areaOfGardenInSqrMeters / minSpacePerPlantInSqrMeters;
+function maxCapacityOfPlants(area = areaOfGardenInSqrMeters) {
+    return area / minSpacePerPlantInSqrMeters;
+};
 
 // Task: Predict the plant growth after a specific number of weeks.
-function plantGrowth(weeks) {
-    return startNumOfPlants  * (plantMultiplierPerWeek ** weeks);
+function plantGrowth(weeks, startNum = startNumOfPlants) {
+    return startNum * (plantMultiplierPerWeek ** weeks);
 };
 
 // Task: Implement control flow to make decisions on whether the plants should be pruned, monitored, or planted:
-function tendPlants(weeks) {
-    plants = plantGrowth(weeks);
+function tendPlants(weeks, startNum, area) {
+    plants = plantGrowth(weeks, startNum);
     let action;
 
-    if (plants > (0.8 * maxCapacityOfPlants)) {
+    if (plants > (0.8 * maxCapacityOfPlants(area))) {
         action = 'Prune';
-    } else if (plants >= (0.5 * maxCapacityOfPlants)) { 
+    } else if (plants >= (0.5 * maxCapacityOfPlants(area))) { 
         action = 'Monitor';
     } else {
         action = 'Plant';
@@ -34,3 +36,17 @@ function tendPlants(weeks) {
 tendPlants(1);
 tendPlants(2);
 tendPlants(3);
+
+
+// Part 2: Thinking Bigger
+
+// Task: determine the amount of additional space that would be required if the scientists were to start with 100 plants, and did not prune them for 10 weeks.
+const startNumOfPlantsIn2ndGarden = 100;
+const numOfPlantsIn2ndGardenAfter10Weeks = plantGrowth(10, startNumOfPlantsIn2ndGarden);
+const areaOf2ndGardenInSqrMeters = numOfPlantsIn2ndGardenAfter10Weeks * minSpacePerPlantInSqrMeters;
+console.log(`Area needed in second garden is ${areaOf2ndGardenInSqrMeters} meters squared`);
+
+// Task: If the space remained circular, what would be the radius of this expanded garden?
+const radiusOf2ndGardenInMeters  = Math.sqrt(areaOf2ndGardenInSqrMeters / Math.PI);
+
+console.log(`The radius of the second garden should be ${radiusOf2ndGardenInMeters.toFixed(2)} meters`);
