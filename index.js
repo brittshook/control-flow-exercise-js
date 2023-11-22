c878100c4549250edc91b7e26d68118ad093cdb6
Britt Shook <brittanysshook@gmail.com>
2023-11-22 18:38:59 -0500

    adjust logic to stop growth when hits capacity and have prune & plant actions affect plant counts

--- index.js
+++ index.js
@@ -1,68 +1,68 @@
 // Part 1: Growing Pains
 
-const areaOfGardenInSqrMeters = (5 ** 2) * Math.PI;
+let areaOfGardenInSqrMeters = (5 ** 2) * Math.PI;
 const minSpacePerPlantInSqrMeters = 0.8;
-const startNumOfPlants = 20;
+let numOfPlants = 20;
 const plantMultiplierPerWeek = 2;
 
 // Calculate max capacity of garden
-function maxCapacityOfPlants(area = areaOfGardenInSqrMeters) {
-    return area / minSpacePerPlantInSqrMeters;
-};
+ maxCapacityOfPlants = Math.floor(areaOfGardenInSqrMeters / minSpacePerPlantInSqrMeters);
 
 // Task: Predict the plant growth after a specific number of weeks.
-function plantGrowth(weeks, startNum = startNumOfPlants) {
-    return startNum * (plantMultiplierPerWeek ** weeks);
-};
-
 // Task: Implement control flow to make decisions on whether the plants should be pruned, monitored, or planted:
-function tendPlants(weeks, startNum, area) {
-    const numOfPlants = plantGrowth(weeks, startNum);
+function tendPlants(weeks, prune = true) {
     let action;
 
-    if (numOfPlants > (0.8 * maxCapacityOfPlants(area))) {
-        action = 'Prune';
-    } else if (numOfPlants >= (0.5 * maxCapacityOfPlants(area))) { 
-        action = 'Monitor';
-    } else {
-        action = 'Plant';
-    };
+    for (let week = 1; week <= weeks; week++) {
+        numOfPlants *= 2;
 
-    return `You should ${action}`;
+        // Plants would naturally stop growing when at capacity, so return to max capacity if over that
+        if (numOfPlants > maxCapacityOfPlants && prune) {
+            numOfPlants = maxCapacityOfPlants;
+        }
+
+        if (numOfPlants > (0.8 * maxCapacityOfPlants) && prune) {
+            numOfPlants -= 5; // Simulate making room for new plants by reducing plant count
+            action = 'Prune';
+        } else if (numOfPlants >= (0.5 * maxCapacityOfPlants)) { 
+            action = 'Monitor';
+        } else {
+            numOfPlants += 5; // Simulate planting by increasing plant count
+            action = 'Plant';
+        };
+    }
+
+    console.log(`Week ${weeks}: ${action}, ${numOfPlants} total plants`);
+    return numOfPlants;
 };
 
 // Show results for 1, 2, and 3 weeks of growth as inputs
-console.log(`After 1 week, there are ${plantGrowth(1)} plants. You should ${tendPlants(1)}.`);
-console.log(`After 2 week, there are ${plantGrowth(2)} plants. You should ${tendPlants(2)}.`);
-console.log(`After 3 week, there are ${plantGrowth(3)} plants. You should ${tendPlants(3)}.`);
+tendPlants(3);
 
 
-// Part 2: Thinking Bigger
+// // Part 2: Thinking Bigger
 
-// Task: determine the amount of additional space that would be required if the scientists were to start with 100 plants, and did not prune them for 10 weeks.
-const startNumOfPlantsIn2ndGarden = 100;
-const numOfPlantsIn2ndGardenAfter10Weeks = plantGrowth(10, startNumOfPlantsIn2ndGarden);
-const areaOf2ndGardenInSqrMeters = numOfPlantsIn2ndGardenAfter10Weeks * minSpacePerPlantInSqrMeters;
+// // Task: determine the amount of additional space that would be required if the scientists were to start with 100 plants, and did not prune them for 10 weeks.
+numOfPlants = 100;
+const numOfPlantsAfter10Weeks = tendPlants(10, false);
+const areaOf2ndGardenInSqrMeters = numOfPlantsAfter10Weeks * minSpacePerPlantInSqrMeters;
 console.log(`Area needed in second garden is ${areaOf2ndGardenInSqrMeters} meters squared.`);
 
-// Task: If the space remained circular, what would be the radius of this expanded garden?
+// // Task: If the space remained circular, what would be the radius of this expanded garden?
 const radiusOf2ndGardenInMeters  = Math.sqrt(areaOf2ndGardenInSqrMeters / Math.PI);
 console.log(`The radius of the second garden should be ${radiusOf2ndGardenInMeters.toFixed(2)} meters.`);
 
 
-// Part 3: Errors in Judgement
-
+// // Part 3: Errors in Judgement
 try {
-    const newStartNum = 100;
-    const newNumOfPlants = plantGrowth(0, newStartNum);
-    const areaNeeded = newNumOfPlants * minSpacePerPlantInSqrMeters;
+    numOfPlants = 100;
+    const areaNeeded = numOfPlants * minSpacePerPlantInSqrMeters;
 
-    if (areaNeeded > (0.8 * maxCapacityOfPlants())) {
-        throw(`${newStartNum} plants will not fit in the original garden.`)
+    if (areaNeeded > (0.8 * maxCapacityOfPlants)) {
+        throw(`${numOfPlants} plants will not fit in the original garden.`)
     } else {
-        console.log(`Yay, all ${newStartNum} plants will fit in the original garden.`)
+        console.log(`Yay, all ${numOfPlants} plants will fit in the original garden.`)
     }
 } catch(err) {
     console.log(err);
 };
-
