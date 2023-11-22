106589bc36a6a748235b29fb749645fe5015167a
Britt Shook <brittanysshook@gmail.com>
2023-11-22 16:45:52 -0500

    fix error in growth calculation

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 // Part 1: Growing Pains
 
-const areaOfPlantCollectionInSqrMeters = (5 ** 2) * Math.PI;
+const areaOfGardenInSqrMeters = (5 ** 2) * Math.PI;
 const minSpacePerPlantInSqrMeters = 0.8;
-const numOfPlantsAtStart = 20;
-const plantMultiplierPerWeek = 2;
+const startNumOfPlants = 20;
+let plantMultiplierPerWeek = 2;
 
 // Calculate max capacity of garden
-const maxCapacityOfPlants = areaOfPlantCollectionInSqrMeters / minSpacePerPlantInSqrMeters;
+const maxCapacityOfPlants = areaOfGardenInSqrMeters / minSpacePerPlantInSqrMeters;
 
 // Task: Predict the plant growth after a specific number of weeks.
 function plantGrowth(weeks) {
-    return numOfPlantsAtStart * (weeks * plantMultiplierPerWeek);
+    return startNumOfPlants  * (plantMultiplierPerWeek ** weeks);
 };
 
 // Task: Implement control flow to make decisions on whether the plants should be pruned, monitored, or planted:
 function tendPlants(weeks) {
-    const plants  = plantGrowth(weeks);
+    plants = plantGrowth(weeks);
+    let action;
 
     if (plants > (0.8 * maxCapacityOfPlants)) {
-        return 'Prune';
-    } else if (plants >= (0.5 * maxCapacityOfPlants)) {
-        return 'Monitor';
+        action = 'Prune';
+    } else if (plants >= (0.5 * maxCapacityOfPlants)) { 
+        action = 'Monitor';
     } else {
-        return 'Plant';
-    }
+        action = 'Plant';
+    };
+
+    console.log(`Total plants after ${weeks} weeks: ${plants}`);
+    console.log(`You should ${action}`);
 };
 
-// Task: Show outputs after 1, 2, and 3 weeks of growth
-console.log(tendPlants(1));
-console.log(tendPlants(2));
-console.log(tendPlants(3));
+// Show results for 1, 2, and 3 weeks of growth as inputs
+tendPlants(1);
+tendPlants(2);
+tendPlants(3);
