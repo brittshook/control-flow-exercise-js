1b7c79bfce94e48c0710f8e5fb512f89c0eb37ea
Britt Shook <brittanysshook@gmail.com>
2023-11-22 19:12:41 -0500

    update logic for tendPlants so it doesnt update a global variiable on each run to avoid issues

--- index.js
+++ index.js
@@ -2,48 +2,51 @@
 
 const areaOfGardenInSqrMeters = (5 ** 2) * Math.PI;
 const minSpacePerPlantInSqrMeters = 0.8;
-let numOfPlants = 20;
+let startNum = 20;
 const plantMultiplierPerWeek = 2;
 
 // Calculate max capacity of garden
- maxCapacityOfPlants = Math.floor(areaOfGardenInSqrMeters / minSpacePerPlantInSqrMeters);
+maxCapacityOfPlants = Math.floor(areaOfGardenInSqrMeters / minSpacePerPlantInSqrMeters);
 
 // Task: Predict the plant growth after a specific number of weeks.
 // Task: Implement control flow to make decisions on whether the plants should be pruned, monitored, or planted:
 function tendPlants(weeks, prune = true) {
     let action;
+    let plants = startNum;
 
     for (let week = 1; week <= weeks; week++) {
-        numOfPlants *= plantMultiplierPerWeek;
+        plants *= plantMultiplierPerWeek;
 
         // Plants would naturally stop growing when at capacity, so return to max capacity if over that
-        if (numOfPlants > maxCapacityOfPlants && prune) {
-            numOfPlants = maxCapacityOfPlants;
+        if (plants > maxCapacityOfPlants && prune) {
+            plants = maxCapacityOfPlants;
         }
 
-        if (numOfPlants > (0.8 * maxCapacityOfPlants) && prune) {
-            numOfPlants -= 5; // Simulate making room for new plants by reducing plant count
+        if (plants > (0.8 * maxCapacityOfPlants) && prune) {
+            plants -= 5; // Simulate making room for new plants by reducing plant count
             action = 'Prune';
-        } else if (numOfPlants >= (0.5 * maxCapacityOfPlants)) { 
+        } else if (plants >= (0.5 * maxCapacityOfPlants)) {
             action = 'Monitor';
         } else {
-            numOfPlants += 5; // Simulate planting by increasing plant count
+            plants += 5; // Simulate planting by increasing plant count
             action = 'Plant';
         };
     }
 
-    console.log(`Week ${weeks}: ${action}, ${numOfPlants} total plants`);
-    return numOfPlants;
+    console.log(`Week ${weeks}: ${action}, ${plants} total plants`);
+    return plants;
 };
 
 // Show results for 1, 2, and 3 weeks of growth as inputs
+tendPlants(1);
+tendPlants(2);
 tendPlants(3);
 
 
 // // Part 2: Thinking Bigger
 
 // // Task: determine the amount of additional space that would be required if the scientists were to start with 100 plants, and did not prune them for 10 weeks.
-numOfPlants = 100;
+startNum = 100;
 const numOfPlantsAfter10Weeks = tendPlants(10, false);
 const areaOf2ndGardenInSqrMeters = numOfPlantsAfter10Weeks * minSpacePerPlantInSqrMeters;
 console.log(`Area needed in second garden is ${areaOf2ndGardenInSqrMeters} meters squared.`);
@@ -55,13 +58,13 @@ console.log(`The radius of the second garden should be ${radiusOf2ndGardenInMete
 
 // // Part 3: Errors in Judgement
 try {
-    numOfPlants = 100;
-    const areaNeeded = numOfPlants * minSpacePerPlantInSqrMeters;
+    startNum = 100;
+    const areaNeeded = startNum * minSpacePerPlantInSqrMeters;
 
     if (areaNeeded > areaOfGardenInSqrMeters) {
-        throw(`${numOfPlants} plants will not fit in the original garden.`)
+        throw(`${startNum} plants will not fit in the original garden.`)
     } else {
-        console.log(`Yay, all ${numOfPlants} plants will fit in the original garden.`)
+        console.log(`Yay, all ${startNum} plants will fit in the original garden.`)
     }
 } catch(err) {
     console.log(err);
